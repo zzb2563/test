cff5c1de24a4ae23b00af7fa52c19b649f48a5de
zhao.xuepu <zhao.xuepu@163.com>
2020-04-19 22:08:29 +0800

    项目整体提交

---
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -0,0 +1,23 @@
+package cn.jiyun.pojo;
+
+public class Student {
+
+    private String userName;
+    private Integer id;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
