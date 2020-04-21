19d54398d21524105690c5a3bc30a2fcda1d5fdd
zhao.xuepu <zhao.xuepu@163.com>
2020-04-21 15:04:21 +0800

    提交 uname

--- src/main/java/cn/jiyun/pojo/Student.java
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -8,6 +8,15 @@ public class Student {
     private String hobby;
     private Integer age;
 
+    private  String  uname;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
 
     public String getUserName() {
         return userName;
